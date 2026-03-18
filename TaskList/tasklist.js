a0bf82f46e21175b2766dfbc2db2ffcf88539ed4
Gianni <BussolettiGianni@gmail.com>
2026-03-18 15:36:44 +0100

    JS done

---
+++ TaskList/tasklist.js
@@ -0,0 +1,37 @@
+const inputTask = document.getElementById("input-list")
+const mainList = document.getElementById("main-list")
+
+inputTask.addEventListener("submit", (e) => {
+  e.preventDefault()
+  dataCollection()
+})
+
+const dataCollection = () => {
+  const input = document.getElementById("text")
+  const okInput = input.value
+  const singleItem = document.createElement("li")
+  singleItem.classList.add("single-item-design")
+  singleItem.innerHTML = `
+  <span onclick="strikeAction(event)" id="strike"><i class="fa-solid fa-pen-to-square"></i></span>
+  ${okInput}
+  <span onclick="deleteAction(event)" id="delete"><i class="fa-solid fa-trash-can"></i></span>`
+  mainList.appendChild(singleItem)
+  inputTask.reset()
+}
+
+let clickCount = 0
+const strikeAction = (event) => {
+  const singleItemStrike = event.target.parentElement.parentElement
+  if (clickCount < 1) {
+    singleItemStrike.classList.add("item-deleted")
+    clickCount += 1
+  } else {
+    singleItemStrike.classList.remove("item-deleted")
+    clickCount--
+  }
+}
+
+const deleteAction = (event) => {
+  const singleItemDelete = event.target.parentElement.parentElement
+  singleItemDelete.remove()
+}
