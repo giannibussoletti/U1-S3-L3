d9cae888a0de4cf86c2459961f5371acf936f76d
Gianni <BussolettiGianni@gmail.com>
2026-03-18 09:30:34 +0100

    Update events.js

--- giorno 3/events.js
+++ giorno 3/events.js
@@ -8,3 +8,21 @@
 
 // Per avere il controllo di questi EVENTI è necessario sistemare delle "TRAPPOLE"
 // Le TRAPPOLE possono catturare ogni evente di un certo elemento -> es. click sui bottone.
+
+// Le trappole in gergo si chiamano "EVENT LISTENERS" (ASCOLTATORI DI EVENTI)
+// Si possono sistemare su qualsiasi elemento della pagina per qualsiasi tipo di evento
+
+// Per impostare un event listeners su un elemento e renderlo ascoltabile, si utilizzano 2 tecniche:
+
+// a) Se avete a disposizione l'elemento in HTML, si può inserire nell'elemento una particolare
+// famiglia di attributi -> "on" + nomeEvento --> es. "onclick", "onscroll" "onmouseenter", etc.
+// a questo attributo verrà assegnata poi l'esecuzione di una funzione
+// es. <button onclick="miaFunzione()">CLICCAMI</button>
+
+// Se l'elemento designato non esiste ancora in HTML e viene creato per esempio tramite
+// document.createElement("button") l'event listenere va inserito in javascript.
+// Per farlo, dovete utilizzare sull'elemento appena creato un metodo JS di nome "addEventListener()"
+
+// Liste  degli eventi
+"https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events"
+"https://www.w3schools.com/jsref/dom_obj_event.asp"
