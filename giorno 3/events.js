ca38054a677dec5fa167ed3f66ceee037258e41c
Gianni <BussolettiGianni@gmail.com>
2026-03-18 09:14:11 +0100

    Initial Commit

---
+++ giorno 3/events.js
@@ -0,0 +1,10 @@
+// EVENTI NEL BROSWER
+// Un evento del broswer è una situazione che accade nella pagina
+// Si può scatenare in automatico oppure dall'interazione da parte dell'utente (click, scroll, hover etc.)
+
+// gli EVENTI accadono sempre all'interno della pagina,
+// anche se la pagina è semplice html,
+// ma di default non scatenano nessun comportamento della pagina
+
+// Per avere il controllo di questi EVENTI è necessario sistemare delle "TRAPPOLE"
+// Le TRAPPOLE possono catturare ogni evente di un certo elemento -> es. click sui bottone.
